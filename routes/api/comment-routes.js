203a6913574c239c2bf8baeef734be2db64e7a9e
unknown <jasmineakbari@yahoo.com>
2021-03-03 20:50:37 -0500

    added reply functionality visibilty and relationship to comments

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { addComment, removeComment, addReply, removeReply } = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
-router.route('/:pizzaId').post(addComment);
+router
+    .route('/:pizzaId')
+    .post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router
+    .route('/:pizzaId/:commentId')
+    .put(addReply)
+    .delete(removeComment);
+
+// deleting route needs a new route since you need to the id of the Reply and 
+// not the parent
+router
+    .route('/:pizzaId/:commentId/:replyId')
+    .delete(removeReply);
 
 module.exports = router;
